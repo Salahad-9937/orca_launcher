94927da1e85c1da0fe793e93daaec3db004e2aa4
Salahad <salahad@kali.kali>
2025-05-28 02:15:12 +0300

    добавлена постоянное сохранение настроек

--- lib/core/models/directory_state.dart
+++ lib/core/models/directory_state.dart
@@ -1,24 +1,48 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DirectoryState extends ChangeNotifier {
   String? _orcaDirectory;
   String? _workingDirectory;
 
-  String? get orcaDirectory =>
-      _orcaDirectory != null ? '$_orcaDirectory/orca' : null;
-  String? get workingDirectory => _workingDirectory;
-
-  void setOrcaDirectory(String? path) {
-    if (_orcaDirectory != path) {
-      _orcaDirectory = path;
-      notifyListeners();
-    }
+  DirectoryState() {
+    _loadPreferences(); // Загружаем сохранённые настройки при инициализации
   }
 
-  void setWorkingDirectory(String? path) {
-    if (_workingDirectory != path) {
-      _workingDirectory = path;
-      notifyListeners();
+  String? get orcaDirectory => _orcaDirectory;
+  String? get workingDirectory => _workingDirectory;
+
+  Future<void> _loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    _orcaDirectory = prefs.getString('orcaDirectory');
+    _workingDirectory = prefs.getString('workingDirectory');
+    notifyListeners();
+  }
+
+  Future<void> setOrcaDirectory(String? path) async {
+    if (path != null) {
+      _orcaDirectory = '$path${Platform.pathSeparator}orca';
+    } else {
+      _orcaDirectory = null;
     }
+    final prefs = await SharedPreferences.getInstance();
+    if (_orcaDirectory != null) {
+      await prefs.setString('orcaDirectory', _orcaDirectory!);
+    } else {
+      await prefs.remove('orcaDirectory');
+    }
+    notifyListeners();
+  }
+
+  Future<void> setWorkingDirectory(String? path) async {
+    _workingDirectory = path;
+    final prefs = await SharedPreferences.getInstance();
+    if (_workingDirectory != null) {
+      await prefs.setString('workingDirectory', _workingDirectory!);
+    } else {
+      await prefs.remove('workingDirectory');
+    }
+    notifyListeners();
   }
 }
